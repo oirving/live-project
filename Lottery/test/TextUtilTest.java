06750c1e972305090379596628a4c83d2c090f0b
murphy-li <murphy-li@foxmail.com>
2019-04-21 18:27:58 +0800

    功能大体正常，数据及表结构在live.sql中

---
+++ Lottery/test/TextUtilTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import lottery.util.TextUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author Murphy
+ * @date 2019/4/21 16:27
+ */
+public class TextUtilTest {
+
+    @Test
+    public void main() throws IOException {
+        String fileName = "Lottery/test/QQrecord-2022.txt";
+        TextUtil.resovle(fileName);
+    }
+}
